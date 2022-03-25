696e2da207cb6b301d730974de45e164e63fa476
Danila Voronkov <danilavoronkov2002@gmail.com>
2022-03-25 15:46:21 +0300

    feat(header): Rotate animation on refresh

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,4 +1,4 @@
-import React, { FC } from 'react';
+import React, { FC, useState } from 'react';
 import styles from "./header.module.scss"
 import Image from 'next/image'
 import Search from "../search/Search";
@@ -8,16 +8,32 @@ const Header: FC = () => {
 
   const queryClient = useQueryClient()
 
+  const [rotate, setRotate] = useState(false);
+
+  const animate = () => {
+
+    // Button begins to shake
+    setRotate(true);
+
+    // Buttons stops to shake after 2 seconds
+    setTimeout(() => setRotate(false), 500);
+  }
+
   return (
     <div className={`${styles.header} container`}>
       <div className={styles.logo}>
         <h1>Список новостей</h1>
-        <button onClick={async () => await queryClient.invalidateQueries("news")}>
-          <Image src="/refresh.svg" alt={"refresh"} width={20} height={16} />
+        <button
+          className={rotate ? `${styles.rotate}` : ""}
+          onClick={async () => {
+            await queryClient.invalidateQueries("news"), animate()
+          }}>
+          <Image className={rotate ? `${styles.rotate}` : ""} src="/refresh.svg" alt={"refresh"} width={20}
+                 height={16}/>
         </button>
       </div>
 
-      <Search />
+      <Search/>
     </div>
   );
 };
